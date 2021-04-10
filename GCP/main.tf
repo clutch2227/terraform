0326d70113101c65b366772f3deb9b5549f324d6
clutch2227 <72704222+clutch2227@users.noreply.github.com>
2021-04-10 19:13:14 -0400

    Delete GCP directory

--- GCP/main.tf
+++
@@ -1,7 +0,0 @@
-module "shared_vpc" {
-  source = "./modules/shared_vpc"
-  cidr_block_public1  = "10.1.0.0/16"
-  cidr_block_public2  = "10.2.0.0/16"
-  cidr_block_private1 = "10.3.0.0/16"
-  cidr_block_private2 = "10.4.0.0/16"
-}
