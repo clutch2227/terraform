0f41ab377acdd0d8c380008f157ce247e188d2ef
clutch2227 <72704222+clutch2227@users.noreply.github.com>
2021-04-10 19:16:51 -0400

    Create variables.tf

---
+++ GCP/modules/shared_vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "cidr_block_public1" {
+    type = string
+    description = "cidr block of public subnet 1"
+}
+
+variable "cidr_block_public2" {
+    type = string
+    description = "cidr block of public subnet 2"
+}
+
+variable "cidr_block_private1" {
+    type = string
+    description = "cidr block of private subnet 1"
+}
+
+variable "cidr_block_private2" {
+    type = string
+    description = "cidr block of private subnet 2"
+}
